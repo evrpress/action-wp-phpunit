77cf2ecbb6f618a73aca84482b9c44f794d1c6a5
Xaver <xaver@everpress.co>
2022-02-03 15:58:03 +0100

    auto commit

--- phpunit.sh
+++ phpunit.sh
@@ -6,3 +6,12 @@
 # it does not exit with a 0, and I only care about the final exit.
 set -eo
 
+ls -a
+
+git clone git://develop.git.wordpress.org/ wordpress-develop
+
+ls -a
+
+cd wordpress-develop
+
+ls -a
